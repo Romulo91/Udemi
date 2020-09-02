32b0a5ae5ea7bd3acbe918ee8e24658aa18b53b7
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-09-02 13:34:05 +0200

    blog

--- React.Redux/components/src/index.js
+++ React.Redux/components/src/index.js
@@ -2,28 +2,41 @@ import React from "react";
 import ReactDom from "react-dom";
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import AppropvalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Rom"
-        timeAgo="Today ar 4:45PM"
-        content="Nice blog post!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Katharina"
-        timeAgo="Today ar 1:00AM"
-        content="nice!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today ar 3:00PM"
-        content="Nice blog post man!"
-        avatar={faker.image.avatar()}
-      />
+      <AppropvalCard>
+        <di>
+          <h4>Warning!</h4>
+          Are you sure you want todo this ?
+        </di>
+      </AppropvalCard>
+      <AppropvalCard>
+        <CommentDetail
+          author="Rom"
+          timeAgo="Today ar 4:45PM"
+          content="Nice blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </AppropvalCard>
+      <AppropvalCard>
+        <CommentDetail
+          author="Katharina"
+          timeAgo="Today ar 1:00AM"
+          content="nice!"
+          avatar={faker.image.avatar()}
+        />
+      </AppropvalCard>
+      <AppropvalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today ar 3:00PM"
+          content="Nice blog post man!"
+          avatar={faker.image.avatar()}
+        />
+      </AppropvalCard>
     </div>
   );
 };
