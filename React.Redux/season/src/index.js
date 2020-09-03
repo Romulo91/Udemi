ea98ab0679711b98bac24a920cae113c73cc3c11
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-09-03 13:20:36 +0200

    new

--- React.Redux/season/src/index.js
+++ React.Redux/season/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hi There!</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDom.render(<App />, document.querySelector("#root"));
